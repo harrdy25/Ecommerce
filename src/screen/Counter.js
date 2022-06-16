972fa8134b7cdeef4b8339d737dfd17be785b0b9
harrdy25 <kukadiya.hardik1@gmail.com>
2022-06-16 11:25:57 +0530

    promise & Firebase

--- src/screen/Counter.js
+++ src/screen/Counter.js
@@ -1,35 +1,113 @@
+// import { View, Text } from 'react-native'
+// import React, { useEffect } from 'react'
+// import { TouchableOpacity } from 'react-native-gesture-handler'
+// import { useDispatch, useSelector } from 'react-redux'
+// import { decrementCounter, incrementCounter } from '../redux/action/counter.action';
+
+// export default function Counter() {
+//     const dispatch = useDispatch();
+//     const count = useSelector(state => state.counter);
+
+//     const handleIncrement = () => {
+//         dispatch(incrementCounter())
+//     }
+
+//     const handleDecrement = () => {
+//         dispatch(decrementCounter())
+//     }
+
+//     useEffect(() => {
+//         printStirng();
+//       }, []);
+
+//       const printStirng = () => {
+//         console.log("Tom");
+//         setTimeout(() => {
+//           console.log("Joy");
+//         }, 2000);
+//         console.log("Jerry");
+//       }
+
+//     return (
+//         <View style={{marginTop: 50}}>
+//             <TouchableOpacity
+//             onPress={() => handleIncrement()}>
+//             <Text>+</Text>
+//             </TouchableOpacity>
+
+//             <Text>{count.count}</Text>
+
+//             <TouchableOpacity
+//             onPress={() => handleDecrement()}>
+//             <Text>-</Text>
+//             </TouchableOpacity>
+
+//         </View>
+//     )
+// }
+
+// Syncronize: One after another, In Order                  // one undefined three
+// Asyncronize: Allow other process to run, Not in order    // one Promises<Pending> three    // axios without async await
+// Promise is asyncronize  
+// async await: Wait for asyncronize call                   // one two (wait for 3 sec) three // axios with async await                                 
+// Callback function is called after working some task.
+// Promises.all resolve all
+
 import { View, Text } from 'react-native'
-import React from 'react'
-import { TouchableOpacity } from 'react-native-gesture-handler'
-import { useDispatch, useSelector } from 'react-redux'
-import { decrementCounter, incrementCounter } from '../redux/action/counter.action';
+import React, { useEffect } from 'react'
 
 export default function Counter() {
-    const dispatch = useDispatch();
-    const count = useSelector(state => state.counter);
-
-    const handleIncrement = () => {
-        dispatch(incrementCounter())
+    const one = () => {
+        return "One Value";
     }
 
-    const handleDecrement = () => {
-        dispatch(decrementCounter())
+    const two = () => {
+        return new Promise((resolve, reject) => {
+            setTimeout(
+                () => {
+                    resolve("Two Value");
+                }, 2000
+            )
+        })
     }
+
+    const three = () => {
+        return "Three Value";
+    }
+
+    const All = () => {
+        const oneVal = one();
+        console.log(oneVal);
+
+        const twoVal = two();
+        console.log(twoVal);
+
+        const threeVal = three();
+        console.log(threeVal);
+    }
+
+    useEffect(
+        () => {
+            All()
+        },
+    [])
+
+    const dispplay = (z) => {
+        console.log(z);
+    }
+    
+    const sum = (callbackFun) => {
+        let x=5, y=3, z;
+        z = x + y;
+
+        callbackFun(z)
+    }
+
+    sum(dispplay);
 
     return (
-        <View style={{marginTop: 50}}>
-            <TouchableOpacity
-            onPress={() => handleIncrement()}>
-            <Text>+</Text>
-            </TouchableOpacity>
-            
-            <Text>{count.count}</Text>
-
-            <TouchableOpacity
-            onPress={() => handleDecrement()}>
-            <Text>-</Text>
-            </TouchableOpacity>
-
+        <View>
+            <Text>Counter</Text>
         </View>
     )
 }
